b0b644425ce206c2408555f837d86ecfcd3a415d
Nakami <natalia.rivas@correo.unimet.edu.ve>
2023-05-19 22:41:00 -0400

    ventana emergente uno hecha :ambulance:

---
+++ index.js
@@ -0,0 +1,11 @@
+const modal=document.querySelector(".modal");
+const cerrarModal=document.querySelector(".modal_close")
+const nombreusuario=document.getElementById("name")
+
+window.addEventListener("load", ()=>{
+
+    setTimeout(
+        ()=>{modal.showModal()},1000
+    )
+            
+    })
