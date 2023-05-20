be3d4704554b882b6cc7616855c0fdacb5270b8e
Nakami <natalia.rivas@correo.unimet.edu.ve>
2023-05-20 18:34:40 -0400

    hay timer :,) :see_no_evil:

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const container=document.querySelector(".card-container");
 const modal=document.querySelector(".modal");
 const cerrarModal=document.querySelector(".modal_close")
-const gameover=document.querySelector(".game_over")
+const gameover=document.querySelector(".anuncio")
 const cuadro=document.querySelector(".cuadro")
 const inicio=document.querySelector(".newgame")
 const start=document.querySelector(".modal_close")
 const user=document.querySelector("#name")
 const info=document.querySelector(".usuario")
 const timer=document.querySelector(".tiempo")
+const textpuntaje=document.querySelector(".puntaje")
 
 
 const imagenes=[
@@ -22,7 +23,8 @@ const imagenes=[
 ];
 
 window.addEventListener("load", ()=>{
-    inicializar()
+    textpuntaje.classList.remove("show")
+    //inicializar()
      setTimeout(
      ()=>{modal.showModal()},100
                 )
@@ -38,20 +40,28 @@ window.onkeydown = function(e){
 inicio.addEventListener("click",()=>{
     seconds=59
     minutes=3
+    faltante=180;
     clearInterval(interval);
+    gameover.innerHTML ="";
     modal.showModal()
 })
 
 start.addEventListener("click", ()=>{
-
     if(user.value!=""){
+    
     setTimeout( ()=> {
-        info.innerHTML=`<h3>Nombre de usuario: ${user.value} </h3>`}
+        info.innerHTML=`<h3>Nombre de usuario: ${user.value} </h3>`
+        textpuntaje.classList.add("show")
+        inicio.classList.add("show")
+        inicializar()
+        
+    }
         ,1000)
     seconds=59;
     minutes=3;
+    faltante=180;
     interval = setInterval(Timer, 1000);  
-    inicializar()
+    
     }  
 })
 
@@ -60,7 +70,7 @@ start.addEventListener("click", ()=>{
 
 let seconds = 59,
 minutes = 3;
-
+let faltante=180;
 const numtarjetas= imagenes.length *2 ;
 let escogido=false;
 let esperar=false;
@@ -72,18 +82,32 @@ let contadortarjetas=0;
 function inicializar() {
     const cartas=mezclar()
     tabla(cartas)
+    
 }
 
 const Timer=() => {
     seconds -= 1;
+    faltante-=1
   //minutes logic
-  if (seconds == 0) {
+
+if( faltante==0) {
+       clearInterval(interval);
+       timer.innerHTML=`<h3>Tiempo: ${00}:${00}</h3>`
+       gameover.innerHTML += `<h2>Juego Terminado</h2>
+                              <h3>Felicidades haz ganado</h3>`;
+       
+
+  }
+
+  else if (seconds == 0) {
     minutes -= 1;
     seconds=59;}
 
   let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
   let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
   timer.innerHTML = `<h3>Tiempo: ${minutesValue}:${secondsValue}</h3>`;
+
+  
 
 }
 
@@ -105,14 +129,13 @@ function mezclar() {
 
 
 function tabla(cartas) {
-    let escogido=false;
-    let esperar=false;
-    let tarjetaabierta;
-    let tarjetaabierta2;
-    let match;
-    let contadortarjetas=0;
+    escogido=false;
+     tarjetaabierta;
+     tarjetaabierta2;
+     match;
+    contadortarjetas=0;
     container.innerHTML="";
-    
+    esperar=false
 
     for(let i=0; i< 4*4; i++) {
 
@@ -136,9 +159,10 @@ function tabla(cartas) {
         (card)=> {
             card.addEventListener("click",
                 ()=>{
+                if(faltante>0) {
                     //todo esto aplicara para cartas que no tenga pareja
                     if (!card.classList.contains("emparejada")) {
-
+                        console.log(esperar)
                         if(!esperar) {
                         card.classList.add("flipped");
                         //si la carta clickeada es la primera
@@ -163,9 +187,10 @@ function tabla(cartas) {
                             contadortarjetas += 1;
                             
                             if (contadortarjetas == (numtarjetas/ 2)) {
-                            
+                                clearInterval(interval);
                               gameover.innerHTML += `<h2>Juego Terminado</h2>
                               <h3>Felicidades haz ganado</h3>`;
+                              esperar=true;
                               //stopGame();
                             }
                           } else {
@@ -184,7 +209,19 @@ function tabla(cartas) {
                         }
                       }
                     }
+                } else if (faltante<=0) {
+                    let [tempFirst, tempSecond] = [tarjetaabierta, tarjetaabierta2];
+                            escogido=false;
+                            tarjetaabierta = null;
+                            tarjetaabierta2 = null;
+                             setTimeout(() => {
+                              tempFirst.classList.remove("flipped");
+                              tempSecond.classList.remove("flipped");
+                              esperar=false;
+                            }, 900);
+
                 }
+            }
             )
         }
     )
